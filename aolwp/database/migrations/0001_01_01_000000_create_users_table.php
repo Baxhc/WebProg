93c1d7e6697fc8719de29b9fb71eae57cd7763d3
NicoleMinarW <nicoleminar@yahoo.com>
2024-12-27 22:13:53 +0700

    test
    test

--- aolwp/database/migrations/0001_01_01_000000_create_users_table.php
+++ aolwp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
-            $table->date('birthday');
-            $table->string('gender');
+            $table->date('birthday')->nullable();
+            $table->string('gender')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
